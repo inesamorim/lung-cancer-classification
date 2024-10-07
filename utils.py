fc647ed3f5ebf9d678d90e16a73a7abdaf0ae081
PsuperX <ja435364@gmail.com>
2024-10-07 19:13:15 +0100

    better image filenames and better malignancy class

--- utils.py
+++ utils.py
@@ -3,19 +3,19 @@ import numpy as np
 from typing import List, Literal
 
 
-def get_malignacy_class(anns: List[pl.Annotation]) -> Literal[-1, 0, 1]:
+def get_nodule_class(anns: List[pl.Annotation]) -> Literal[-1, 0, 1]:
+    malign = round(np.mean([ann.malignancy for ann in anns]))
+    return get_malignancy_class(malign)
+
+classes = [0, -1, -1, 0, 1, 1]
+def get_malignancy_class(malign: int) -> Literal[-1, 0, 1]:
     """
     1 - Sus ඞ
     0 - ¯\_(ツ)_/¯
     -1 - no sus
     """
 
-    classes = [0, -1, -1, 0, 1, 1]
-    malign = np.mean([ann.malignancy for ann in anns], dtype=int)
     return classes[malign]
 
-# TODO: _nodule_id is weird
 def filename_from_annotation(ann: pl.Annotation) -> str:
-    return ann.scan.patient_id + "-" + ann._nodule_id
-
-
+    return ann.scan.patient_id + "-" + str(ann.id)
