51828c94a61d49eb7e3586f0c9fdf8934d37f12c
inesamorim <inessamorim46@gmail.com>
2024-10-08 11:55:51 +0100

    radiomics ready to run (i hope)

--- utils.py
+++ utils.py
@@ -2,6 +2,11 @@ import pylidc as pl
 import numpy as np
 from scipy.spatial.distance import cdist
 from typing import List, Literal
+from pylidc.utils import consensus
+import matplotlib.pyplot as plt
+import os
+from pathlib import Path
+
 
 
 def get_nodule_class(anns: List[pl.Annotation]) -> Literal[-1, 0, 1]:
@@ -20,6 +25,8 @@ def get_malignancy_class(malign: int) -> Literal[-1, 0, 1]:
 
 def filename_from_annotation(ann: pl.Annotation) -> str:
     return ann.scan.patient_id + "-" + str(ann.id)
+
+#########################################################################################################
 
 def _group_nodules_by_proximity(annotations: List[pl.Annotation], distance_threshold: float):
     """ Group nodules manually based on spatial proximity using centroid distance. """
@@ -83,3 +90,50 @@ def cluster_annots(scan: pl.Scan, distance_threshold: float=10.0, verbose: bool=
         mprint("################################################")
     
     return nodules
+
+
+
+#######################################################################################################################
+
+def zoomout_nodule(bbox, width, height, n_slices):
+    # TODO: what if is outside the picture?
+    x_acres = (width - (bbox[0].stop - bbox[0].start))//2
+    y_acres = (height - (bbox[1].stop - bbox[1].start))//2
+
+    x_slice = slice(bbox[0].start - x_acres, bbox[0].start - x_acres + width, None)
+    y_slice = slice(bbox[1].start - y_acres, bbox[1].start - y_acres + width, None)
+
+    z_slice = np.linspace(bbox[2].start, bbox[2].stop, n_slices, endpoint=False, dtype=int)
+
+    return (x_slice, y_slice, z_slice)
+
+def get_cropped_annot(scan: pl.Scan, nods, mask: bool=False):
+    if len(scan.annotations) == 0:
+        return None
+
+    vol = scan.to_volume(verbose=False)
+
+    images = []
+    masks = []
+    for anns in nods:
+        cmask, cbbox, _ = consensus(anns, clevel=0.5,
+                                pad=[(10,10), (10,10), (0,0)])
+
+        full_mask = np.full_like(vol, False, dtype=bool)
+        full_mask[cbbox] = cmask
+
+        #cbbox = zoomout_nodule(cbbox, MAX_NODULE_WIDTH, MAX_NODULE_HEIGHT, N_SLICES)
+        cropped = vol[cbbox]
+        cropped_mask = full_mask[cbbox]
+        if mask:
+            cropped[cropped_mask] = -1000 # TODO: maybe pick different value
+
+        # Scale values
+        # TODO: maybe pick different values
+        cropped += 1000
+        cropped = cropped / 2000
+
+        images.append(cropped)
+        masks.append(cropped_mask)
+
+    return images, masks
